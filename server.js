77b21dac8f149c6e900c19dbd770180130b4041f
Valtteri Ranta <waltherbeach@gmail.com>
2020-10-01 18:53:27 +0300

    Added colors and morgan for dev logging

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 // Load environment variables
@@ -11,11 +13,18 @@ connectDB();
 
 const app = express();
 
+// Logging middleware for development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
