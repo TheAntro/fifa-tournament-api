e5549ce2731a22b3892f26c172239f5ac05eb6b5
Valtteri Ranta <waltherbeach@gmail.com>
2020-10-05 19:41:29 +0300

    routes for Teams CRUD functionality

--- routes/teams.js
+++ routes/teams.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { getTeams } = require('../controllers/teams');
+const {
+  getTeams,
+  getTeam,
+  createTeam,
+  updateTeam,
+  deleteTeam,
+} = require('../controllers/teams');
 
 const router = express.Router();
 
-router.route('/').get(getTeams);
+router.route('/').get(getTeams).post(createTeam);
+
+router.route('/:id').get(getTeam).put(updateTeam).delete(deleteTeam);
 
 module.exports = router;
